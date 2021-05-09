6e2912c6ed0f30194decf473cb3b03485b53e49e
Lanerxx <892907311@qq.com>
2021-05-09 11:46:28 +0800

    设置定时执行器

--- src/main/java/com/example/springbootgraduationdesign/component/MyTimerComponent.java
+++ src/main/java/com/example/springbootgraduationdesign/component/MyTimerComponent.java
@@ -2,6 +2,7 @@ package com.example.springbootgraduationdesign.component;
 
 import com.example.springbootgraduationdesign.service.CompanyService;
 import com.example.springbootgraduationdesign.service.StudentService;
+import com.example.springbootgraduationdesign.service.TrackService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.scheduling.annotation.Scheduled;
@@ -10,14 +11,24 @@ import org.springframework.stereotype.Component;
 @Component
 @Slf4j
 public class MyTimerComponent {
-//    @Autowired
-//    private StudentService studentService;
-//    @Autowired
-//    private CompanyService companyService;
-//
-//    @Scheduled(cron = "0 0 2 * * *")
-//    public void timingMatching(){
-////        studentService.getJobMatchResult();
-////        companyService.getStudentMatchResult();
-//    }
+    @Autowired
+    private StudentService studentService;
+    @Autowired
+    private CompanyService companyService;
+    @Autowired
+    private TrackService trackService;
+
+
+    @Scheduled(cron = "0 0 2 * * *")
+    public void timingTracking(){
+        trackService.getJobSMRSystemDefaultWeight();
+        trackService.getResumeJMRSystemDefaultWeight();
+    }
+
+    @Scheduled(cron = "0 0 3 * * *")
+    public void timingMatching(){
+        studentService.getResumeJMR_Match();
+        companyService.getJobSMR_Match();
+    }
+
 }
